ad6736844fb7f9512b1973c2a5694533baf45c7c
Konstantinos Giotis <kostas.yiotis@hotmail.com>
2023-12-06 14:32:08 +0200

    add title

--- web-app/uni-todo-app/src/App.tsx
+++ web-app/uni-todo-app/src/App.tsx
@@ -20,6 +20,7 @@ function App() {
   return (
     <>
       <div className="h-100 w-full flex flex-col items-center justify-center bg-teal-lightest font-sans">
+        MY UNI TODO
         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
           <TodoListHeader />
           <TodoListBody />
